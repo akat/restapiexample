4a6ba8128f00313dd116ca01d9dfb6dac9a78f8c
akat <akatsaris@gmail.com>
2019-06-04 16:40:01 +0300

    update routes

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ const path = require('path');
 
 module.exports = (app, express) => {
 	const router = express.Router();
+	
+	//Set default api path
+	app.use('/api', router);
+	
 	Routes.forEach(route => {
 		route(router)
 	})
 
-	//Set default api prefix
-	app.use('/api', router);
-
+	app.get('/', (req, res) => {
+		res.send({ver: '1.0.0'})
+	})
+	
 	// 404 not found
 	app.use('*', (req, res) => {
 		res.sendFile(path.join(__dirname+'/pages/404.html'));
